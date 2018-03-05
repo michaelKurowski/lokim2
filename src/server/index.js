be167bca18506ef9c05255c8c74acfad57d70f82
michaelKurowski <Byamarro@gmail.com>
2018-03-05 13:46:21 +0100

    Remove login route leftovers

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const login = require('./routes/login')
 const register = require('./routes/register')
 const bodyParser = require('body-parser')
 const config = require('./config.json')
@@ -8,7 +7,6 @@ const app = express()
 
 app.use(bodyParser.json())
 
-app.use('/login',login)
 app.use('/register',register)
 
 app.listen(config.httpServer.port, (err)=> {
