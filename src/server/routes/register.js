7727575f3073a0edabb6b90f5f14ba78e74579b2
marcin grzeszczak <marcin.grzeszczak.2@gmail.com>
2018-03-08 19:34:53 +0100

    Added login option

---
+++ src/server/routes/register.js
@@ -0,0 +1,34 @@
+
+const express = require('express')
+const router = express.Router()
+const UserModel = require('../models/user')
+const logger = require('../logger.js')
+const Utilities = require('../utilities')
+const errorMessages = require('./utilities/errorMessages')
+const statusCodes = require('./utilities/statusCodes')
+const SALT_SIZE = 70
+
+router.post('/', (req, res) => {
+	
+	const salt = Utilities.generateSalt(SALT_SIZE)
+	const password = Utilities.createSaltedHash(salt, req.body.password)
+
+	const userData = {
+		username: req.body.username,
+		email: req.body.email,
+		password,
+		salt
+	}
+
+	const userInstance = new UserModel(userData)
+
+	userInstance.save()
+		.then( () => res.status(statusCodes.OK).send('Created new user'))
+		.catch( err => {
+			logger.error(err)
+			return res.status(statusCodes.BAD_REQUEST)
+				.send(Utilities.createError(errorMessages.BAD_REQUST))
+		})
+})
+
+module.exports = router
