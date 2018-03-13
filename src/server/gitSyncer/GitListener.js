b1ad86a66e10373e37b7da7c8b435ebb64daa587
Michael Kurowski <Byamarro@gmail.com>
2018-03-13 22:42:50 +0100

    Added GitListener#pullBranch

--- src/server/gitSyncer/GitListener.js
+++ src/server/gitSyncer/GitListener.js
@@ -1,14 +1,18 @@
 const gitMessages = require('./gitMessages')
 const logger = require('../logger')
 const config = require('../config.json')
+const spawn = require('child_process').spawn
+const path = require('path')
 
+const CURRENT_PATH = path.resolve(__dirname)
 const PORT = config.gitIntegration.listeningPort
+const BRANCH = config.gitIntegration.branch
 const GIT_EVENT_IDENTIFIER_HTTP_HEADER = 'X-GitHub-Event'
 const GIT_PUSH_EVENT = 'push'
 
 class GitListener {
 	handleHttpRequests(request, response) {
-		const branchToListen = `refs/heads/${config.gitIntegration.branch}`
+		const branchToListen = `refs/heads/${BRANCH}`
 		const body = request.body
 		const eventType = request.get(GIT_EVENT_IDENTIFIER_HTTP_HEADER)
 
@@ -29,6 +33,23 @@ class GitListener {
 			return
 		}
 		logger.info(`Git monitor is listening on port ${PORT}`)
+	}
+
+	pullBranch() {
+		return new Promise( (resolve, reject) => {
+			const COMMAND_CONFIG = {cwd: CURRENT_PATH}
+			const ARGUMENTS = ['pull', '--force', 'origin', BRANCH]
+			const COMMAND = 'git'
+
+			const gitPull = spawn(COMMAND, ARGUMENTS, COMMAND_CONFIG)
+
+			gitPull.stdout.on('data', resolve)
+			gitPull.on('error', reject)
+			gitPull.on('close', code => {
+				if (code !== 0) reject(`git pull failed, and closed with code: ${code}`)
+			})
+		})
+
 	}
 }
 
