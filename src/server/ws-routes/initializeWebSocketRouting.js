bb53239a15a66ed90dcd742fe0e61c07c2f5d033
Michael Kurowski <Byamarro@gmail.com>
2018-04-07 22:24:47 +0200

    initializeWebSocketRouting cleanup

--- src/server/ws-routes/initializeWebSocketRouting.js
+++ src/server/ws-routes/initializeWebSocketRouting.js
@@ -6,7 +6,5 @@ const Utilities = require('../utilities')
 module.exports = initializeWebSocketRouting
 
 function initializeWebSocketRouting(io, protocol = require('../protocol/protocol.json')) {
-
 	Utilities.createWebsocketRoute(io, protocol.room, require('./controllers/Room'), new ConnectionsRepository())
-
 }
