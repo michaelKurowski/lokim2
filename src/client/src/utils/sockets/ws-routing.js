789ec4befcf3ee827f11c1141a77dfb2c0411a15
Jordan Moss <jmoss636@gmail.com>
2018-05-04 20:58:29 +0100

    removing unnecessary code

--- src/client/src/utils/sockets/ws-routing.js
+++ src/client/src/utils/sockets/ws-routing.js
@@ -1,12 +1,6 @@
 const io = require('socket.io-client')
 const IO_CONNECTION_URL = 'localhost:5000/room' /* Take this from config file in the future */
-const socket = io(IO_CONNECTION_URL, {
-    path: '/connection',
-    reconnection: true,
-    reconnectionDelay: 1000,
-    reconnectionDelayMax : 5000,
-    reconnectionAttempts: Infinity
-})
+const socket = io(IO_CONNECTION_URL, {path: '/connection'})
 
 module.exports = socket
 
