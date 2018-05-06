2a0538065aa55b4e7841c561cc84369159ea8765
Jordan Moss <jmoss636@gmail.com>
2018-05-06 21:40:59 +0100

    Enzyming app.test.js

--- src/client/src/.tests/components/App.test.js
+++ src/client/src/.tests/components/App.test.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import {shallow, mount, configure} from 'enzyme'
+import {mount, configure} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import App from '../../App';
-import {BrowserRouter as Router, Route} from 'react-router'
 
 configure({adapter: new Adapter()})
 
 describe('<App />', () => {
-  const wrapper = shallow(<App />)
+  const wrapper = mount(<App />)
 
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
+   expect(wrapper.find(App).length).toBe(1)
   });
 })
 
