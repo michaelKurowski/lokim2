7cd9c61908024cd1a960824e5331733fa5f22207
Michael Kurowski <Byamarro@gmail.com>
2018-05-12 23:58:40 +0200

    Added exiting with error code 1 upon issues in application

--- src/server/index.js
+++ src/server/index.js
@@ -2,7 +2,9 @@ const init = require('./init')
 
 
 init().catch(error => {
+	const FAILURE_EXIT_CODE = 1
 	const logger = require('./logger')
 	logger.error('Failed to run application: ', error.message, 'for details set logLevel config variable to "debug"')
 	logger.debug('Error details', error)
+	process.exit(FAILURE_EXIT_CODE)
 })
