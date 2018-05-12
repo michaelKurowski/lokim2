b9969aa155bf5faf4695728f65a00ab869aaf0c1
Jordan Moss <jmoss636@gmail.com>
2018-05-12 18:52:14 +0100

    Forgot one

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -4,10 +4,10 @@ const HomePage = require('./components/homepage')
 const Register = require('./components/register')
 const ChatPage = require('./components/chatpage')
 const {BrowserRouter, Route} = require('react-router-dom')
-
-const LOGIN_URL = '/'
-const REGISTER_URL = '/register'
-const CHAT_URL = '/chat'
+const {urls} = require('./routes/routes')
+const LOGIN_URL = urls.LOGIN
+const REGISTER_URL = urls.REGISTER
+const CHAT_URL = urls.CHAT
 
 class App extends React.Component {
 	render() {
