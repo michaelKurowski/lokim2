adc2c4fece738263595154c7919121cdf97ccfc7
Michael Kurowski <Byamarro@gmail.com>
2018-05-14 20:48:30 +0200

    Extracting ws tool js code draft

---
+++ src/server/.tests/tools/webSocketsTester.js
@@ -0,0 +1,25 @@
+const wst = (() => {
+	const USERNAME_INPUT_ID = 'username'
+	const PASSWORD_INPUT_ID = 'password'
+	const NAMESPACE_INPUT_ID = 'address'
+	const EVENTS_TO_LISTEN_TABLE_ID = 'events-to-listen'
+	const INCOMMING_TRAFFIC_TABLE_ID = 'ws-table'
+
+	function getUsername() {
+		return document.getElementById(USERNAME_INPUT_ID).value
+	}
+
+	function getPassword() {
+
+	}
+
+	function getNamespace(){
+
+	}
+
+
+
+	return {
+
+	}
+})()
