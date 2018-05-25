30e194fbb8da8739e160d20b596de3e7bfcecdae
Michael Kurowski <Byamarro@gmail.com>
2018-05-25 20:20:34 +0200

    Refactor of /Users namespace UTs

--- src/server/.tests/ws-routes/controllers/Users.spec.js
+++ src/server/.tests/ws-routes/controllers/Users.spec.js
@@ -15,60 +15,60 @@ describe('Users websocket namespace', () => {
 	afterEach(() => {
 		sandbox.restore()
 	})
-	describe('unit tests', () => {
+
+	describe('#find', () => {
 		beforeEach(() => {
 			suite.mongooseQuery = sinon.stub()
 		})
-		describe('#find', () => {
-			it('should call a response to client who sent event', () => {
-				//given
-				const QUERY_PHRASE = 'dummyUsername'
-				const QUERY_FEEDBACK_MOCK = []
+		it('should call a response to client who sent event', () => {
+			//given
+			const QUERY_PHRASE = 'dummyUsername'
+			const QUERY_FEEDBACK_MOCK = []
 
-				const socketMock = {emit: sinon.spy()}
-				const queryResultMock = {exec: sinon.stub().resolves(QUERY_FEEDBACK_MOCK)}
-				sandbox.stub(UserModel, 'find').returns(queryResultMock)
-				suite.usersInstance = new UsersProvider(UserModel)
-				//when
-				return suite.usersInstance.find({queryPhrase: QUERY_PHRASE}, socketMock)
-					.then(() => {
-						//then
-						sinon.assert.calledOnce(socketMock.emit)
-					})
-			})
+			const socketMock = {emit: sinon.spy()}
+			const queryResultMock = {exec: sinon.stub().resolves(QUERY_FEEDBACK_MOCK)}
+			sandbox.stub(UserModel, 'find').returns(queryResultMock)
+			suite.usersInstance = new UsersProvider(UserModel)
+			//when
+			return suite.usersInstance.find({queryPhrase: QUERY_PHRASE}, socketMock)
+				.then(() => {
+					//then
+					sinon.assert.calledOnce(socketMock.emit)
+				})
+		})
 
-			it('should send back users found in database', () => {
-				//given
-				const QUERY_PHRASE = 'dummyUsername'
+		it('should send back users found in database', () => {
+			//given
+			const QUERY_PHRASE = 'dummyUsername'
 
-				const FOUND_USERNAME1 = 'dummyUsername'
-				const FOUND_USERNAME2 = 'dummyUsername2'
-				const QUERY_FEEDBACK_MOCK = [
-					{
-						$id: 'fakeMongoDBId',
-						username: FOUND_USERNAME1
-					},
-					{
-						$id: 'fakeMongoDBId2',
-						username: FOUND_USERNAME2
-					}
-				]
-
-				const EXPECTED_DATA_TO_BE_SEND_TO_USER = {
-					foundUsernames: [FOUND_USERNAME1, FOUND_USERNAME2]
+			const FOUND_USERNAME1 = 'dummyUsername'
+			const FOUND_USERNAME2 = 'dummyUsername2'
+			const QUERY_FEEDBACK_MOCK = [
+				{
+					$id: 'fakeMongoDBId',
+					username: FOUND_USERNAME1
+				},
+				{
+					$id: 'fakeMongoDBId2',
+					username: FOUND_USERNAME2
 				}
+			]
 
-				const socketMock = {emit: sinon.spy()}
-				const queryResultMock = {exec: sinon.stub().resolves(QUERY_FEEDBACK_MOCK)}
-				sandbox.stub(UserModel, 'find').returns(queryResultMock)
-				suite.usersInstance = new UsersProvider(UserModel)
-				//when
-				return suite.usersInstance.find({queryPhrase: QUERY_PHRASE}, socketMock)
-					.then(() => {
-						//then
-						sinon.assert.calledWith(socketMock.emit, EVENT_TYPES.FIND, EXPECTED_DATA_TO_BE_SEND_TO_USER)
-					})
-			})
+			const EXPECTED_DATA_TO_BE_SEND_TO_USER = {
+				foundUsernames: [FOUND_USERNAME1, FOUND_USERNAME2]
+			}
+
+			const socketMock = {emit: sinon.spy()}
+			const queryResultMock = {exec: sinon.stub().resolves(QUERY_FEEDBACK_MOCK)}
+			sandbox.stub(UserModel, 'find').returns(queryResultMock)
+			suite.usersInstance = new UsersProvider(UserModel)
+			//when
+			return suite.usersInstance.find({queryPhrase: QUERY_PHRASE}, socketMock)
+				.then(() => {
+					//then
+					sinon.assert.calledWith(socketMock.emit, EVENT_TYPES.FIND, EXPECTED_DATA_TO_BE_SEND_TO_USER)
+				})
 		})
 	})
+
 })
