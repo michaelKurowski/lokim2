1cb439f490bbfd6de37934884a85f066bf5482ac
michaelKurowski <Byamarro@gmail.com>
2018-06-16 16:28:25 +0200

    init.js refactor

--- src/server/init.js
+++ src/server/init.js
@@ -1,112 +1,35 @@
-const cors = require('cors')
 const express = require('express')
 const app = express()
 const httpServer = require('http').Server(app)
-const bodyParser = require('body-parser')
-
-const io = require('socket.io')(httpServer, {path: '/connection'})
-const passport = require('passport')
-const passportSocketIo = require('passport.socketio')
 const expressSession = require('express-session')
-const LocalStrategy = require('passport-local').Strategy
-const mongoSanitize = require('express-mongo-sanitize')
-const util = require('util')
 const MongoSessionStore = require('connect-mongo')(expressSession)
-let logger
+
+const initializationUtils = require('./initializationUtils')
 async function init({
-	httpPort
+	injectedHttpPort
 } = {}) {
-
-	const configFileService = require('./configFileService')()
-	const isConfigExisting = await configFileService.isConfigFileExisting()
-	if (isConfigExisting) {
-		configFileService.validateFields(require('./config.json'))
-		logger = require('./logger')
-	} else {
-		await configFileService.generateConfig()
-		logger = require('./logger')
-		logger.info('No config file found, new config generated. Please fill it.')
-		configFileService.validateFields(require('./config.json'))
-		
-	}
-
+	await initializationUtils.initializeConfig()
 	const config = require('./config.json')
-	httpPort = process.env.PORT || httpPort || config.httpServer.port
+
+	//Imports that need config file to initialize
 	const router = require('./routes/router')
-	const webSocketRouting = require('./ws-routes/webSocketRouting')
-	const passportStrategies = require('./passport/strategies')
-	const passportStrategyUtils = require('./passport/strategyUtils')
 	const dbConnection = require('./dbConnection')
+
 	const sessionStore = new MongoSessionStore({ mongooseConnection: dbConnection} )
-	const cookieSession = {
-		store: sessionStore,
-		secret: config.session.secret, 
-		resave: config.session.resave, 
-		saveUninitialized: config.session.saveUninitialized,
-		cookie: { 
-			secure: config.session.cookie.secure,
-			maxAge: config.session.cookie.maxAge
-		}
-	}
+	const cookieHttpSessionConfig = initializationUtils.createCookiesHttpSessionConfigObject(config, sessionStore)
+	const cookieWebsocketSessionConfig = initializationUtils.createCookiesWebsocketSessionConfigObject(config, sessionStore)
 
-	const websocketCookieSession = {
-		key: config.session.cookieName,
-		secret: config.session.secret,
-		store: sessionStore,
-		success: (data, accept) => {
-			if (config.devPropeties.devMode) logger.debug('New websocket connection estabilished')
-			accept()
-		},
-		fail: (data, msg, err, accept) => {
-			if (config.devPropeties.devMode) logger.debug('Failed to connect via websocket connection', msg)
-			accept(new Error(err))
-		}
-	}
+	initializationUtils.initializeHttpRequestProcessingFlow(app, router, cookieHttpSessionConfig)
+	initializationUtils.initializeWebSocketEventProcessingFlow(httpServer, cookieWebsocketSessionConfig)
+	initializationUtils.initializePassport()
 
-	//Express flow
-	app.use(cors({credentials: true, origin: 'http://localhost:3000'}))
-	app.use(bodyParser.json())
-	app.use(mongoSanitize())
-	app.use(expressSession(cookieSession))
-	app.use(passport.initialize())
-	app.use(passport.session())
-	app.use(router)
-
-	//Passport setting
-
-	const loginStrategy = passportStrategies.loginStrategy()
-	passport.serializeUser(loginStrategy.serializeUser)
-	passport.deserializeUser(loginStrategy.deserializeUser)
-
-	passport.use(passportStrategyUtils.STRATEGY_NAME, new LocalStrategy({
-		usernameField: passportStrategyUtils.FIELDS_NAMES.USERNAME_FIELD,
-		passwordField: passportStrategyUtils.FIELDS_NAMES.PASSWORD_FIELD
-	}, loginStrategy.validateUser))
-
-	//Initialize http server
-	const startHttpServer = httpServer.listen.bind(httpServer)
-	const httpServerListening = util.promisify(startHttpServer)(httpPort)
-		.then(() => {
-			logger.info(`HTTP server is listening on port ${httpPort}`)
-			return httpServer
-		})
-		.catch(err => {
-			logger.error(`Error during attempting to listen for port ${httpPort}`)
-			return err
-		})
-
-	//websocket flow
-	io.use(passportSocketIo.authorize(websocketCookieSession))
-	webSocketRouting.initializeWebSocketRouting(io)
+	const httpPort = process.env.PORT || injectedHttpPort || config.httpServer.port
+	const httpServerListening = initializationUtils.initializeHttpServer(httpServer, httpPort)
 
 	return {
-		app,
-		passport,
-		io,
 		httpServerListening,
 		dbConnection
 	}
 }
-
 
 module.exports = init
