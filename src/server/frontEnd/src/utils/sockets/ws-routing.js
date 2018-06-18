e382f6c50e563263e825f24eeb8f22ada8142044
michaelKurowski <Byamarro@gmail.com>
2018-06-18 21:50:11 +0200

    Cleaned from comment

--- src/server/frontEnd/src/utils/sockets/ws-routing.js
+++ src/server/frontEnd/src/utils/sockets/ws-routing.js
@@ -1,5 +1,5 @@
 const io = require('socket.io-client')
-const IO_CONNECTION_URL = `${document.location.host}/room` /* Take this from config file in the future */
+const IO_CONNECTION_URL = `${document.location.host}/room`
 const socket = io(IO_CONNECTION_URL, {path: '/connection'})
 
 module.exports = socket
