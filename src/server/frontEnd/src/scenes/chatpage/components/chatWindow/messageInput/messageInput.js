c2a2b4ae4ca490bdab56b2b6c40c04ee7dcaa6fc
michaelKurowski <Byamarro@gmail.com>
2018-07-10 00:39:54 +0200

    You can now send messages with 'Enter' key

--- src/server/frontEnd/src/scenes/chatpage/components/chatWindow/messageInput/messageInput.js
+++ src/server/frontEnd/src/scenes/chatpage/components/chatWindow/messageInput/messageInput.js
@@ -8,6 +8,7 @@ class MessageInput extends React.Component {
 		}
 		this.handleUserInput = this.handleUserInput.bind(this)
 		this.sendMessage = this.sendMessage.bind(this)
+		this.onSubmit = this.onSubmit.bind(this)
 	}
 
 	sendMessage() {
@@ -18,12 +19,16 @@ class MessageInput extends React.Component {
 		this.setState({input: event.target.value})
 	}
 
+	onSubmit(event) {
+		event.preventDefault()
+	}
+
 	render() {
 		return (
-			<div className='p-2'>
+			<form className='p-2' onSubmit={this.onSubmit}>
 				<input className='form-control' placeholder='Message...' onChange={this.handleUserInput}/>
 				<button className='btn btn-primary' value={this.state.input} onClick={this.sendMessage}>Send</button>
-			</div>
+			</form>
 		)
 	}
 }
