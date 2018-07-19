ae3a0018d69c1982bd87a6894722394ac7549a9a
michaelKurowski <Byamarro@gmail.com>
2018-07-20 01:37:11 +0200

    Minor refactor

--- src/server/connectToDb.js
+++ src/server/connectToDb.js
@@ -8,6 +8,7 @@ module.exports = function (username, password, hostname) {
 	const db = username ?
 		mongoose.createConnection(`mongodb://${username}:${password}@${hostname}`) :
 		mongoose.createConnection(`mongodb://${hostname}`)
+
 	db.on('error', err => logger.error(`Failed to connect to database with host "${hostname}". Error: ${err}`))
 	db.once('open', () => logger.info(`Connected to database: ${hostname}`))
 	return db
