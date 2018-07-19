15bbf5599578aee8891ae093d80a80737cf449a0
michaelKurowski <Byamarro@gmail.com>
2018-07-19 19:06:17 +0200

    Added handling failed registration requests

--- src/server/frontEnd/src/scenes/register/register.js
+++ src/server/frontEnd/src/scenes/register/register.js
@@ -3,6 +3,7 @@ const {Link, Redirect} = require('react-router-dom')
 const _fetch = require('node-fetch')
 const {urls, paths} = require('../../routes/routes.json')
 const SUCCESS_CODE = 200
+const USER_ALREADY_EXIST = 400
 const REGISTER_URL = urls.REGISTER
 const HOMEPAGE_PATH = paths.HOME
 const POST = 'POST'
@@ -18,29 +19,35 @@ class Register extends React.Component {
 			email: '',
 			successfulRegister: false
 		}
-		this.handleChange = this.handleChange.bind(this)
-		this.handleSubmit = this.handleSubmit.bind(this)
+		this.updateCredentials = this.updateCredentials.bind(this)
+		this.registerUser = this.registerUser.bind(this)
 	}
-	handleChange(event) {
+	updateCredentials(event) {
 		this.setState({ [event.target.name] : event.target.value})
 	}
     
-	handleSubmit(event) {
+	registerUser(event) {
 		this.registerHandler(this.state)
 		event.preventDefault()
 	}
 	registerHandler(userData, fetch) {
-		fetch = fetch || /* istanbul ignore next */ _fetch
+		fetch = fetch || _fetch
 		const {username, password, email} = userData
 		fetch(REGISTER_URL, {
 			method: POST, headers,
 			body: JSON.stringify({username, password, email})
 		}).then(response => {
-			/* istanbul ignore next */ 
-			if(response.status === SUCCESS_CODE) {
-				this.setState({successfulRegister: true})
+			switch(response.status) {
+				case SUCCESS_CODE:
+					this.setState({successfulRegister: true})
+					break
+				case USER_ALREADY_EXIST:
+					alert('User already exist')
+					break
+				default:
+					console.error(new Error(`Unexpected answer to registration request: ${response.status}`))
 			}
-		}).catch(err => err)
+		}).catch(err => console.error(err))
 	}
 	render() {
 		if(this.state.successfulRegister) {
@@ -49,10 +56,10 @@ class Register extends React.Component {
 		return (
 			<div className='container-fluid register-div'>
 				<h2> Register for a new LokIM Account </h2>
-				<form onSubmit={this.handleSubmit}>
-					<input type='text' className='user-input' placeholder='Username' value={this.state.username} onChange={this.handleChange} name='username' required/><br/>
-					<input type='password' className='user-input' placeholder='Password' value={this.state.password} onChange={this.handleChange} name='password' required/><br/>
-					<input type='text' className='user-input' placeholder='Email' value={this.state.email} onChange={this.handleChange} name='email' required/><br/>
+				<form onSubmit={this.registerUser}>
+					<input type='text' className='user-input' placeholder='Username' value={this.state.username} onChange={this.updateCredentials} name='username' required/><br/>
+					<input type='password' className='user-input' placeholder='Password' value={this.state.password} onChange={this.updateCredentials} name='password' required/><br/>
+					<input type='text' className='user-input' placeholder='Email' value={this.state.email} onChange={this.updateCredentials} name='email' required/><br/>
 					<input type='submit' className='btn btn-primary register-button' value='Register'/>
 					<Link className='btn btn-secondary' to={HOMEPAGE_PATH}>Go Back</Link>
 				</form>   
