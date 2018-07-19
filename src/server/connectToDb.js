b3f90184650c840962caf95aa8bb89a238470235
michaelKurowski <Byamarro@gmail.com>
2018-07-20 01:36:49 +0200

    Added ability to connect to Mongo without credentials

--- src/server/connectToDb.js
+++ src/server/connectToDb.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose')
 const logger = require('./logger')
 
 module.exports = function (username, password, hostname) {
-	const db = mongoose.createConnection(`mongodb://${username}:${password}@${hostname}`)
+	if (!username) logger.warn('No username is given for MongoDB connection')
+	if (!password) logger.warn('No password is given for MongoDB connection')
+
+	const db = username ?
+		mongoose.createConnection(`mongodb://${username}:${password}@${hostname}`) :
+		mongoose.createConnection(`mongodb://${hostname}`)
 	db.on('error', err => logger.error(`Failed to connect to database with host "${hostname}". Error: ${err}`))
 	db.once('open', () => logger.info(`Connected to database: ${hostname}`))
 	return db
