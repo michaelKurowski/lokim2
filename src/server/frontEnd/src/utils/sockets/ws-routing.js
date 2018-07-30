61738c103099e07f48ce02e26cf16fc2aea44f1e
michaelKurowski <Byamarro@gmail.com>
2018-07-30 22:21:45 +0200

    ws-routing cleanup

--- src/server/frontEnd/src/utils/sockets/ws-routing.js
+++ src/server/frontEnd/src/utils/sockets/ws-routing.js
@@ -1,15 +1,11 @@
 const io = require('socket.io-client')
 const _ = require('lodash')
-const serverProvidedProtocols = require('../../../../protocol/protocol.json')
-const actions = require('../../services/webSocket/webSocket.actions').actions
 
 const ROOM = `room`
 const USERS = `users`
 const HOST = `${document.location.host}/`
-const protocols = require('../io-protocol.json')
-
+const WEBSOCKET_ENDPOINT = '/connection'
 let socket
-let dispatch
 
 module.exports = {
 	create,
@@ -23,33 +19,12 @@ function inject(injectedDispatch) {
 
 function create() {
 	socket = {
-		room: io(`${HOST}${ROOM}`, {path: '/connection'}),
-		users: io(`${HOST}${USERS}`, {path: '/connection'})
+		room: io(`${HOST}${ROOM}`, {path: WEBSOCKET_ENDPOINT}),
+		users: io(`${HOST}${USERS}`, {path: WEBSOCKET_ENDPOINT})
 	}
-	//setupListeners()
 	return socket
 }
 
 function get() {
 	return socket
 }
-
-function setupListeners() {
-	_.forEach(serverProvidedProtocols, namespace => 
-		_.forEach(namespace.eventTypes, eventType => 
-			socket[namespace.name].on(eventType, event => 
-				dispatch(actions.webSocketEvent({event, type: eventType}))
-			)
-		)
-	)
-	
-	socket.room.on(protocols.CONNECTION, event => dispatch(actions.room.connected(event)))
-	socket.room.on(protocols.MESSAGE, event => dispatch(actions.room.messageReceived(event)))
-	socket.room.on(protocols.JOIN, event => dispatch(actions.room.userJoined(event)))
-	socket.room.on(protocols.LIST_MEMBERS, event => dispatch(actions.room.membersListed(event)))
-
-	socket.users.on(protocols.CONNECTION, event => dispatch(actions.users.connected(event)))
-	socket.users.on(protocols.FIND, event => dispatch(actions.users.usersFound(event)))
-}
-
-
