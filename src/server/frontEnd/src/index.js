f3249a4d3e21829be1826251b4dd6074ae53f356
michaelKurowski <Byamarro@gmail.com>
2018-08-01 16:41:07 +0200

    Extracted redux initialization to a separated file

--- src/server/frontEnd/src/index.js
+++ src/server/frontEnd/src/index.js
@@ -5,25 +5,11 @@ const React  = require('react')
 const ReactDOM = require('react-dom')
 const App = require('./theme/App')
 
-const {createStore, applyMiddleware, combineReducers} = require('redux')
 const {Provider} = require('react-redux')
-const roomsManagementReducer = require('./services/roomsManagement/roomsManagement.reducer')
-const sessionReducer = require('./services/session/session.reducer')
-const {composeWithDevTools} = require('redux-devtools-extension')
-const isDevMode = require('../../config.json').devPropeties.devMode
-const createSagaMiddleware = require('redux-saga').default
-const watchLogIn = require('./services/sagas/logIn.saga').watchLogIn
-const webSocketListener = require('./services/sagas/webSocketListener.saga').watch
-const webSocketEmitter = require('./services/sagas/webSocketEmitters.saga').watch
-const sagaMiddleware = createSagaMiddleware()
-const initialMiddleware = [sagaMiddleware]
+const initializeRedux = require('./initializeRedux')
 
-const middleware = isDevMode ? composeWithDevTools(applyMiddleware(...initialMiddleware)) : applyMiddleware(...initialMiddleware)
-const rootReducer = combineReducers({roomsManagementReducer, sessionReducer})
-const store = createStore(rootReducer, middleware)
-sagaMiddleware.run(webSocketListener)
-sagaMiddleware.run(watchLogIn)
-sagaMiddleware.run(webSocketEmitter)
+const store = initializeRedux()
+
 ReactDOM.render(
     <Provider store={store} >
         <App/>
