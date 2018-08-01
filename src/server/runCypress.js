0df1180950c7f6718b8edf252894876e2c453045
michaelKurowski <Byamarro@gmail.com>
2018-08-02 00:25:23 +0200

    Parallel lokim building and cypresss execution

--- src/server/runCypress.js
+++ src/server/runCypress.js
@@ -1,6 +1,3 @@
 const spawn = require('child_process').spawn
 const lokIMInitialization = require('./init')()
-Promise.all([
-	lokIMInitialization.dbConnection,
-	lokIMInitialization.httpServerListening
-]).then(() => spawn('npx', ['cypress', 'run'], {cwd: __dirname}))
+lokIMInitialization.then(() => spawn('npx', ['cypress', 'run'], {cwd: __dirname}))
