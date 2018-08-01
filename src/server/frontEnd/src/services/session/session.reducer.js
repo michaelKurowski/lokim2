c7f8fbb2241751c2c603f47dda154a04a33e4f71
michaelKurowski <Byamarro@gmail.com>
2018-08-02 00:06:44 +0200

    Tests fixed

--- src/server/frontEnd/src/services/session/session.reducer.js
+++ src/server/frontEnd/src/services/session/session.reducer.js
@@ -11,22 +11,21 @@ const initialState = {
 }
 
 const handleActions = (state = initialState, action = {}) => {
-	console.log(action)
 	switch (action.type) {
-	case WEBSOCKET_ACTION_CODES.WEBSOCKET_CONNECTION_ESTABILISHED:
-		if (action.payload.namespace === 'room') return Object.assign({}, state, {isConnectedToRoom: true})
-		if (action.payload.namespace === 'users') return Object.assign({}, state, {isConnectedToUsers: true})
-		return state
-	case SESSION_ACTION_CODES.LOG_IN_PENDING:
-		return Object.assign({}, state, {status: STATES.PENDING, errorMessage: '', username: action.payload.username, reason: ''})
-	case SESSION_ACTION_CODES.AUTHORISE:
-		return Object.assign({}, state, {status: STATES.SUCCEDED, errorMessage: '', username: action.payload.username, reason: ''})
-	case SESSION_ACTION_CODES.DENY_AUTHORISATION:
-		return Object.assign({}, state, {status: STATES.FAILED, errorMessage: action.payload.error, username: action.payload.username, reason: action.payload.reason})
-	case SESSION_ACTION_CODES.LOG_OUT:
-		return initialState
-	default:
-		return state
+		case WEBSOCKET_ACTION_CODES.WEBSOCKET_CONNECTION_ESTABILISHED:
+			if (action.payload.namespace === 'room') return Object.assign({}, state, {isConnectedToRoom: true})
+			if (action.payload.namespace === 'users') return Object.assign({}, state, {isConnectedToUsers: true})
+			return state
+		case SESSION_ACTION_CODES.LOG_IN_PENDING:
+			return Object.assign({}, state, {status: STATES.PENDING, errorMessage: '', username: action.payload.username, reason: ''})
+		case SESSION_ACTION_CODES.AUTHORISE:
+			return Object.assign({}, state, {status: STATES.SUCCEDED, errorMessage: '', username: action.payload.username, reason: ''})
+		case SESSION_ACTION_CODES.DENY_AUTHORISATION:
+			return Object.assign({}, state, {status: STATES.FAILED, errorMessage: action.payload.error, username: action.payload.username, reason: action.payload.reason})
+		case SESSION_ACTION_CODES.LOG_OUT:
+			return initialState
+		default:
+			return state
 	}
 }
 
