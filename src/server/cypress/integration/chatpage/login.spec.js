ccc3d372d397b1f5fb188b657c632579fa28d558
michaelKurowski <Byamarro@gmail.com>
2018-08-01 21:03:52 +0200

    Added cypress to CI

--- src/server/cypress/integration/chatpage/login.spec.js
+++ src/server/cypress/integration/chatpage/login.spec.js
@@ -1,23 +1,23 @@
 /// <reference types="Cypress" />
 
 context('Login', () => {
-    beforeEach(() => {
-        cy.visit('http://localhost:5000')
-    })
+	beforeEach(() => {
+		cy.visit('http://localhost:5000')
+	})
 
-    it('can login', () => {
-        cy.get('[name="username"]').type('test_user')
-        cy.get('[name="password"]').type('test_password')
-        cy.get('input[value="Login"]').click()
-        cy.url().should('include', '/chat')
-        cy.get('.success')
-    })
+	it('can login', () => {
+		cy.get('[name="username"]').type('test_user')
+		cy.get('[name="password"]').type('test_password')
+		cy.get('input[value="Login"]').click()
+		cy.url().should('include', '/chat')
+		cy.get('.success')
+	})
 
-    it('can logout', () => {
-        cy.get('[name="username"]').type('test_user')
-        cy.get('[name="password"]').type('test_password')
-        cy.get('input[value="Login"]').click()
-        cy.get('a.btn.btn-danger').click()
-        cy.url().should('include', '/')
-    })
+	it('can logout', () => {
+		cy.get('[name="username"]').type('test_user')
+		cy.get('[name="password"]').type('test_password')
+		cy.get('input[value="Login"]').click()
+		cy.get('a.btn.btn-danger').click()
+		cy.url().should('include', '/')
+	})
 })
