1ebc16f8529851d4cb7342a82a00f5a297c26450
michaelKurowski <Byamarro@gmail.com>
2018-08-05 21:02:20 +0200

    Cypress login tests refactor

--- src/server/.tests/cypress/integration/chatpage/login.spec.js
+++ src/server/.tests/cypress/integration/chatpage/login.spec.js
@@ -1,38 +1,43 @@
 /// <reference types="Cypress" />
-
+let suite
 context('Login', () => {
 	beforeEach(() => {
+		suite = {
+			CORRECT_USERNAME: 'test_username',
+			CORRECT_PASSWORD: 'test_password',
+			WRONG_USERNAME: 'test_user222'
+		}
 		cy.visit('http://localhost:5000')
 	})
 
 	it('can register', () => {
 		cy.get('.home-button.btn.btn-secondary a').click()
 		cy.url().should('include', '/register')
-		cy.get('[name="username"]').type('test_username')
-		cy.get('[name="password"]').type('test_password')
+		cy.get('[name="username"]').type(suite.CORRECT_USERNAME)
+		cy.get('[name="password"]').type(suite.CORRECT_PASSWORD)
 		cy.get('[name="email"]').type('testEmail@test.co.uk')
 		cy.get('.register-button').click()
 		cy.url().should('include', '/')
 	})
 
 	it('can login', () => {
-		cy.get('[name="username"]').type('test_user')
-		cy.get('[name="password"]').type('test_password')
+		cy.get('[name="username"]').type(suite.CORRECT_USERNAME)
+		cy.get('[name="password"]').type(suite.CORRECT_PASSWORD)
 		cy.get('input[value="Login"]').click()
 		cy.url().should('include', '/chat')
 		cy.get('.success')
 	})
 
 	it('can see message when login fails', () => {
-		cy.get('[name="username"]').type('test_user222')
-		cy.get('[name="password"]').type('test_password')
+		cy.get('[name="username"]').type(suite.WRONG_USERNAME)
+		cy.get('[name="password"]').type(suite.CORRECT_PASSWORD)
 		cy.get('input[value="Login"]').click()
 		cy.get('.alert-danger')
 	})
 
 	it('can logout', () => {
-		cy.get('[name="username"]').type('test_user')
-		cy.get('[name="password"]').type('test_password')
+		cy.get('[name="username"]').type(suite.CORRECT_USERNAME)
+		cy.get('[name="password"]').type(suite.CORRECT_PASSWORD)
 		cy.get('input[value="Login"]').click()
 		cy.get('a.btn.btn-danger').click()
 		cy.url().should('include', '/')
