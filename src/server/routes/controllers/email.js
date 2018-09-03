57e80fe0b37cd83b958457ebd0f26ccb84f09389
Jordan Moss <jmoss636@gmail.com>
2018-09-03 15:38:52 +0100

    createToken improvement

--- src/server/routes/controllers/email.js
+++ src/server/routes/controllers/email.js
@@ -1,12 +1,14 @@
 const crypto = require('crypto')
 const HASH_ALGORITHM = 'sha256'
 const DIGEST_METHOD = 'hex'
-const SECRET = process.env.SECRET_KEY || config.SECRET_KEY || 'supersecretkey'
 
 function createToken(){
-    const hash = crypto.createHmac(HASH_ALGORITHM, SECRET)
-                .digest(DIGEST_METHOD)
-    return hash
+    const currentDate = (new Date().valueOf().toString())
+    const random = Math.random().toString()
+
+    const hash = crypto.createHash(HASH_ALGORITHM)
+    const token = hash.update(currentDate + random).digest(DIGEST_METHOD)
+    return token
 }
 
 module.exports.createToken = createToken
