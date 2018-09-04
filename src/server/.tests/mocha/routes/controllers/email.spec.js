2f40a2c8d2b339efc3b5db31890a9b5636ff059e
Jordan Moss <jmoss636@gmail.com>
2018-09-04 20:11:24 +0100

    Email test revision

--- src/server/.tests/mocha/routes/controllers/email.spec.js
+++ src/server/.tests/mocha/routes/controllers/email.spec.js
@@ -8,26 +8,35 @@ const DUMMY_SUBJECT = 'subject'
 const DUMMY_BODY = 'bodybodybody'
 const NO_EMAIL = null
 const EXPECTED_ERROR_MESSAGE = 'Receive address, subject or body cannot be undefined.'
+const BUFFER_ALLOC_SIZE = 20
+const BUFFER_VALUE = 2
+const HEX = 'hex'
 
 describe('email controller', () => {
 	beforeEach(() => {
         suite = {}
     })
 
-    describe('Token Creation', () => {
-        // beforeEach(() => {
-        //     suite.DUMMY_USERID = '_A12345'
-        //     suite.DUMMY_USERNAME = 'Rick'
-        //     suite.DUMMY_PASSWORD = 'ImPickleRick'
-        //     suite.DUMMY_EMAIL = 'morty@ricks-confederation.com'
-        // })
-        it('Should create a valid token based on Math.random and current date', () => {
-            sinon.stub(Math, 'random').returns(4)
-            sinon.stub(Date).returns(1535984556)
+    describe('Sandboxed Token Creation', () => {
+        let sandbox
+
+        beforeEach(() => {
+            sandbox = sinon.sandbox.create()
+        })
+
+        afterEach(() => {
+            sandbox.restore()
+        })
+        it('Should create a valid token of length 20', () => {
+            const buf1 = Buffer.alloc(BUFFER_ALLOC_SIZE, BUFFER_VALUE)
+            sinon.stub(crypto, 'randomBytes').returns(buf1)
+
             const generatedToken = emailController.createToken()
-            const EXPECTED_TOKEN = null; //ADD LATER #FIXME
+            const EXPECTED_TOKEN = buf1.toString(HEX)
+            const EXPECTED_LENGTH = 20
 
             assert.strictEqual(generatedToken, EXPECTED_TOKEN)
+            assert.strictEqual(generatedToken.length, EXPECTED_LENGTH)
         })
         it('Should return a different value every time', () => {
             const generatedToken1 = emailController.createToken()
