35de9d96c650d54baf89017019fb12ef626bee06
Jordan141 <jmoss636@gmail.com>
2018-10-24 17:03:26 +0100

    linting

--- src/server/models/verification.js
+++ src/server/models/verification.js
@@ -6,16 +6,16 @@ const Schema = mongoose.Schema
 const DUPLICATE_KEY_ERROR_DESC = 'There was a duplicate key'
 
 const emailSchema = new Schema({
-    username:{
-        type: String,
-        require: true,
-        unique: true
-    },
-    token: {
-        type: String,
-        required: true,
-        unique: true
-    }
+	username:{
+		type: String,
+		require: true,
+		unique: true
+	},
+	token: {
+		type: String,
+		required: true,
+		unique: true
+	}
 })
 
 emailSchema.plugin(uniqueValidator, {message: DUPLICATE_KEY_ERROR_DESC})
--- src/server/routes/controllers/email.js
+++ src/server/routes/controllers/email.js
@@ -11,108 +11,108 @@ const USER_NOT_FOUND = 'User not found.'
 const account = config.email
 
 const SMTP_OPTIONS = {
-    host: `smtp.${account.hostname}`,
-    port: account.PORT,
-    secure: false,
-    auth: {
-        user: account.email,
-        pass: account.password
-    }
+	host: `smtp.${account.hostname}`,
+	port: account.PORT,
+	secure: false,
+	auth: {
+		user: account.email,
+		pass: account.password
+	}
 }
 
 async function prepareTransporter() {
-    let transporter = nodemailer.createTransport(SMTP_OPTIONS)
+	let transporter = nodemailer.createTransport(SMTP_OPTIONS)
 
-    await transporter.verify((err) => {
-        if(err) return logger.warn(`Transporter Verification Error: ${err}`)
-        logger.info('Email server is ready to take our messages')
-    })
+	await transporter.verify((err) => {
+		if(err) return logger.warn(`Transporter Verification Error: ${err}`)
+		logger.info('Email server is ready to take our messages')
+	})
 
-    return transporter
+	return transporter
 }
 
 function createToken() {
-    const token = crypto.randomBytes(20).toString('hex')
-    return token
+	const token = crypto.randomBytes(20).toString('hex')
+	return token
 }
 
 function setMailOptions(recvAddress, subject, body) {
-    return {
-        from: LOKIM_EMAIL,
-        to: recvAddress,
-        subject,
-        text: body
-    }
+	return {
+		from: LOKIM_EMAIL,
+		to: recvAddress,
+		subject,
+		text: body
+	}
 }
 
 function sendMail(transporter = prepareTransporter()) {
-    return (mailOptions) => {
-        _.forEach(mailOptions, optionValue => {
-            if(_.isEmpty(optionValue)) throw new Error('Mail options cannot be undefined.')
-        })
+	return (mailOptions) => {
+		_.forEach(mailOptions, optionValue => {
+			if(_.isEmpty(optionValue)) throw new Error('Mail options cannot be undefined.')
+		})
 
-        transporter.sendMail(mailOptions, (err, info) => {
-            if(err) return logger.warn(`Email failed to send. Details: ${err}`)
-            logger.info('Message sent: %s', info.messageId)
-        })
-    }
+		transporter.sendMail(mailOptions, (err, info) => {
+			if(err) return logger.warn(`Email failed to send. Details: ${err}`)
+			logger.info('Message sent: %s', info.messageId)
+		})
+	}
 }
 
 function saveRecordToDB(VerifyModel = require('../../models/verification')) {
-    return (username, token) => {
-        if(_.isEmpty(username) || _.isEmpty(token)) throw new Error('You must provide a username and verification token.')
+	return (username, token) => {
+		if(_.isEmpty(username) || _.isEmpty(token)) throw new Error('You must provide a username and verification token.')
 
-        const verifyData = {username, token}
-        const verifyInstance = new VerifyModel(verifyData)
+		const verifyData = {username, token}
+		const verifyInstance = new VerifyModel(verifyData)
         
-        return verifyInstance.save().catch(err => {throw new Error('Error creating Verify record. Please ensure details are unique. More details: ' + err)})//TODO: Handle this better
-    }
+		return verifyInstance.save().catch(err => {throw new Error('Error creating Verify record. Please ensure details are unique. More details: ' + err)})//TODO: Handle this better
+	}
 }
 
 function sendVerificationMail(transporter = prepareTransporter()) {
-    return (email, hostname, verificationToken) => {
-        if(_.isEmpty(email) || _.isEmpty(hostname) || _.isEmpty(verificationToken))
-            throw new Error('You must provide a hostname, email and verification token.')
+	return (email, hostname, verificationToken) => {
+		if(_.isEmpty(email) || _.isEmpty(hostname) || _.isEmpty(verificationToken))
+			throw new Error('You must provide a hostname, email and verification token.')
 
-        const link =`${hostname}/verify/${verificationToken}`
-        const subject = 'Verification Email'
-        const body = `You can activate your account at ${link} \nIt will expire after 30  days.`
+		const link =`${hostname}/verify/${verificationToken}`
+		const subject = 'Verification Email'
+		const body = `You can activate your account at ${link} \nIt will expire after 30  days.`
 
-        sendMail(transporter)(setMailOptions(email, subject, body))
-    }
+		sendMail(transporter)(setMailOptions(email, subject, body))
+	}
 }
 
 function verifyUser(
-    Verify = require('../../models/verification'),
-    User = require('../../models/user')) {
-    return (req) => {
-        const token = req.params.token
+	Verify = require('../../models/verification'),
+	User = require('../../models/user')) {
+	return (req) => {
+		const token = req.params.token
         
-        if(_.isEmpty(token))
-            throw new Error(INVALID_TOKEN)
+		if(_.isEmpty(token))
+			throw new Error(INVALID_TOKEN)
 
-        return Verify.find({token}, (err, foundUser) => {
-            if(err)
-                throw new Error(INVALID_TOKEN)
+		return Verify.find({token}, (err, foundUser) => {
+			if(err)
+				throw new Error(INVALID_TOKEN)
 
-            const {username} = foundUser
+			const {username} = foundUser
 
-            return User.findOneAndUpdate({username}, {active: true}, (err) => { 
-                if (err)
-                    throw new Error(USER_NOT_FOUND)
+			return User.findOneAndUpdate({username}, {active: true}, (err) => { 
+				if (err)
+					throw new Error(USER_NOT_FOUND)
                     
-                return Verify.remove({token})
-            })
-        })
-    }
+				return Verify.remove({token})
+			})
+		})
+	}
 }
 
 module.exports = { 
-    createToken,
-    mailOptions: setMailOptions,
-    sendMail,
-    saveRecordToDB,
-    sendVerificationMail,
-    prepareTransporter,
-    verifyUser
+	createToken,
+	mailOptions: setMailOptions,
+	sendMail,
+	saveRecordToDB,
+	sendVerificationMail,
+	prepareTransporter,
+	verifyUser
 }
