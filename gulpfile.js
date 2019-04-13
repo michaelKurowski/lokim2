81e2b834363f37dcb76c2f1265c959d87fef466d
obsceniczny <mr.cisco@protonmail.com>
2019-04-14 01:43:20 +0200

    Exuitting process upon db connection error

--- gulpfile.js
+++ gulpfile.js
@@ -157,8 +157,10 @@ function frontEndEslintAutoFix(cb) {
 }
 
 function serverTest(cb) {
-    const childProcess = spawn(`${PATHS.MOCHA} './.tests/mocha/**/*.spec.js'`, {cwd: PATHS.SERVER, shell: true, stdio: 'inherit', detached: false})
+    //console.log(`${PATHS.MOCHA} './.tests/mocha/**/*.spec.js'`)
+    const childProcess = spawn('npx mocha "./.tests/mocha/**/*.spec.js"', {cwd: PATHS.SERVER, shell: true, stdio: 'inherit', detached: false})
     childProcess.on('close', (data) => {
+        childProcess.kill()
         cb()
     })
     childProcess.on('error', (data) => {
