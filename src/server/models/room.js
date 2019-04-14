df618cc4c8d555f4c25be1ce19617794c8838cff
Jordan Moss <jmoss636@gmail.com>
2019-04-14 18:51:18 +0100

    Room Model changes

--- src/server/models/room.js
+++ src/server/models/room.js
@@ -3,22 +3,20 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const roomSchema = new Schema({
+    id: {
+        type: 
+    },
     name: {
         type: String,
         required: true
     },
     messages: [{author: String, body: String, date: Date}],
-    creationDate: {type: Date, default: Date.now}
-    /**
-     * At this stage it makes sense to move the list of users in the room to the database for accessibility.
-     * however I will not be commenting this out until a discussion has been held, as this is a major change to existing infrastucture
-     * 
-     *  */ 
-    // users: [{
-    //     userId: {type: mongoose.Schema.ObjectId, required: true},
-    //     name: String,
-    //     joinDate: {type: Date, default: Date.now}
-    // }]
+    creationDate: {type: Date, default: Date.now},
+    users: [{
+        userId: {type: mongoose.Schema.ObjectId, required: true},
+        name: String,
+        joinDate: {type: Date, default: Date.now}
+    }]
 })
 
 
