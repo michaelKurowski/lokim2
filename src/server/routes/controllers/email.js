695aae80875c34e4189f377163017046c87658da
Jordan Moss <jmoss636@gmail.com>
2019-04-20 04:41:18 +0100

    refactor verifyUser

--- src/server/routes/controllers/email.js
+++ src/server/routes/controllers/email.js
@@ -89,26 +89,30 @@ function verifyUser(
 			return INVALID_TOKEN
 		}
 
-		return Verify.findOne({token}, (err, foundUser) => {
-			if(err || foundUser === null) {
+		const username = findUsername(token, Verify)
+		
+		if(username === null){
+			res.redirect('/email-is-invalid')
+			return INVALID_TOKEN
+		}
+
+		return User.findOneAndUpdate({username}, {$set: {active: true}}, (err) => { 
+			if (err) {
+				logger.error(USER_NOT_FOUND, req)
 				res.redirect('/email-is-invalid')
-				return INVALID_TOKEN
+				return USER_NOT_FOUND
 			}
-				
-
-			const {username} = foundUser
-
-			return User.findOneAndUpdate({username}, {$set: {active: true}}, (err) => { 
-				if (err) {
-					logger.error(USER_NOT_FOUND, req)
-					res.redirect('/email-is-invalid')
-					return USER_NOT_FOUND
-				}
-				res.redirect('/email-is-valid')
-				return Verify.remove({token})
-			})
+			res.redirect('/email-is-valid')
+			return Verify.remove({token})
 		})
 	}
+}
+
+function findUsername(token, Verify){
+	return Verify.findOne({token}, (err, foundUser) => {
+		if(err || foundUser === null) return null
+		return foundUser.username
+	})
 }
 
 module.exports = { 
