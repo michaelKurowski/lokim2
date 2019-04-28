c9b2fb64773d7398d88dadef3465bc1e49da3dac
Damian Lupa <damian.lupa88@gmail.com>
2019-04-28 12:03:16 +0200

    fix problems with files

--- src/server/frontEnd/src/index.js
+++ src/server/frontEnd/src/index.js
@@ -1,17 +1,17 @@
 
 import registerServiceWorker from './registerServiceWorker'
-const React  = require('react')
+const React = require('react')
 const ReactDOM = require('react-dom')
 const App = require('./theme/App')
 
-const {Provider} = require('react-redux')
+const { Provider } = require('react-redux')
 const initializeRedux = require('./initializeRedux')
 
 const store = initializeRedux()
 
 ReactDOM.render(
 	<Provider store={store} >
-		<App/>
+		<App />
 	</Provider>
 	, document.getElementById('root'))
 registerServiceWorker()
