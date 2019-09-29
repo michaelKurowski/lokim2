767ba23c7d6c590eb7de8fcbd9f6c996d4f6ba99
MarcinGrzeszczak <marcin.grzeszczak@gmail.com>
2019-09-29 18:04:04 +0200

    Fixed linter errors

--- src/server/frontEnd/src/.tests/theme/components/Avatar.spec.js
+++ src/server/frontEnd/src/.tests/theme/components/Avatar.spec.js
@@ -3,12 +3,10 @@ const {configure, shallow} = require('enzyme')
 const Adapter = require('enzyme-adapter-react-16')
 const AvatarComponent = require('../../../theme/components/avatar/avatar')
 
-let suite = {}
 configure({ adapter: new Adapter() })
-describe('<Avatar />', ()=>{
-    it.only('correct avatar render', ()=>{
-        const wrapper = shallow(<AvatarComponent size='48' username='rick' />)
-        expect(wrapper).toMatchSnapshot()
-    })
-
+describe('<Avatar />', () => {
+	it('correct avatar render', () => {
+		const wrapper = shallow(<AvatarComponent size='48' username='rick' />)
+		expect(wrapper).toMatchSnapshot()
+	})
 })
