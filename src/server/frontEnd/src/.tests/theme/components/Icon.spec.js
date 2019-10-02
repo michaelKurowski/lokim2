468cff065b0767f996691321638f77e45f0ec43c
MarcinGrzeszczak <marcin.grzeszczak@gmail.com>
2019-10-02 17:58:00 +0200

    Cahnged to Icon component

--- src/server/frontEnd/src/.tests/theme/components/Icon.spec.js
+++ src/server/frontEnd/src/.tests/theme/components/Icon.spec.js
@@ -1,12 +1,12 @@
 const React = require('react')
 const {configure, shallow} = require('enzyme')
 const Adapter = require('enzyme-adapter-react-16')
-const AvatarComponent = require('../../../theme/components/icon/icon')
+const IconComponent = require('../../../theme/components/icon/icon')
 
 configure({ adapter: new Adapter() })
 describe('<Icon />', () => {
 	it('correct icon render', () => {
-		const wrapper = shallow(<AvatarComponent size='48' icon='logo' />)
+		const wrapper = shallow(<IconComponent size='48' icon='logo' />)
 		expect(wrapper).toMatchSnapshot()
 	})
 })
