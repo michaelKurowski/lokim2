9a5f3dc5891393ea0cf90ddaa58781c88f21b242
michaelKurowski <Byamarro@gmail.com>
2019-10-04 00:22:44 +0200

    leave button stylistics

--- src/server/frontEnd/src/theme/scenes/chatpage/components/roomsDialer/roomsListElement/roomsListElement.js
+++ src/server/frontEnd/src/theme/scenes/chatpage/components/roomsDialer/roomsListElement/roomsListElement.js
@@ -11,11 +11,11 @@ class RoomsListElement extends React.Component {
 	}
 	render() {
 		return(
-			<div className='border m-2 p-2' >
+			<div className='border m-2 p-2 flex justify-between' >
 				<div data-test='list-dialer-element' key={this.state.ID} onClick={this.props.onClick}>
 					<li>{this.state.name}</li>
 				</div>
-				<div onClick={this.props.onLeave}>Leave</div>
+				<div className='bg-primary text-light p-1' onClick={this.props.onLeave}>Leave</div>
 			</div>
 
 		)
