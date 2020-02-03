70a022f9a062992989ef7eadafe672787cd70ae4
MarcinGrzeszczak <marcin.grzeszczak@gmail.com>
2020-02-03 01:04:52 +0100

    Fixed eslint errors

--- src/server/models/pendingInvitations.js
+++ src/server/models/pendingInvitations.js
@@ -3,14 +3,14 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const pendingInvitationsSchema = new Schema({
-    from: {
-        type:String,
-        required:true
-    },
-    to: {
-        type:String,
-        required:true
-    }
+	from: {
+		type:String,
+		required:true
+	},
+	to: {
+		type:String,
+		required:true
+	}
 })
 
 const PendingInvitationsModel = dbConnection.model('pendingInvitations', pendingInvitationsSchema)
